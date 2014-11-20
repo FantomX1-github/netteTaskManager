7d9b8f2f817b78be34dd416e753f96c624abef92
Frank Hallo <fantomx1@gmail.com>
2014-11-20 16:48:59 +0100

    dokumentacia a upratanie

--- app/presenters/CreatePresenter.php
+++ app/presenters/CreatePresenter.php
@@ -3,144 +3,144 @@
 namespace App\Presenters;
 
 use Nette;
+/**
+ * Presenter class for task manipulation, creation, editing, displaying
+ * @author František Hallo
+ */
+class CreatePresenter extends BasePresenter {
 
+  /** @var Nette\Database\Context */
+  private $database;
 
-class CreatePresenter extends BasePresenter
-{
-    /** @var Nette\Database\Context */
-    private $database;
+  public function __construct(Nette\Database\Context $database) {
+    $this->database = $database;
+  }
 
-    public function __construct(Nette\Database\Context $database)
-    {
-        $this->database = $database;
+/**
+ * Method is here just to call invalidate data for new task template snippet
+ * 
+ */
+  public function actionNew() {
+
+    if ($this->isAjax())
+      $this->invalidateControl('content');
+
+  }
+
+/**
+ * Assigns data to edit form template
+ * 
+ */
+  public function actionEdit($taskId) {
+
+    if ($this->isAjax())
+      $this->invalidateControl('content');
+
+    $task = $this->database->table('task')->get($taskId);
+    if (!$task) {
+      $this->error('Task neexistuje');
     }
-
-
-    public function renderDefault(){
-
-      
-
-      
-    }
-    public function actionNew(){
-      
-      if ($this->isAjax())
-        $this->invalidateControl('content');
-
-      $aTasks = $this->database->table('task');
-
-      $this->template->aTasks = $aTasks;
-
-      
-
-      
-    }
-    
-
-    public function actionEdit($taskId)
-    {
-      
-      if ($this->isAjax())
-        $this->invalidateControl('content');
-      
-      $task = $this->database->table('task')->get($taskId);
-      if (!$task) {
-          $this->error('Task neexistuje');
-      }
-      $this['taskEditForm']->setDefaults($task->toArray());
+    $this['taskEditForm']->setDefaults($task->toArray());
 //      $this['taskEditForm']->setDefaults(array('time'=>''));
+  }
+
+  
+ /**
+ * Creating reusable-minimalistic task form
+ * 
+ */
+  protected function createComponentTaskForm() {
+    $form = new Nette\Application\UI\Form;
+
+    $form->addText('name', 'Názov:')
+        ->setRequired();
+
+    $form->addText('desc', 'Popis:');
+
+    $form->addSubmit('send', 'Uložiť');
+    $form->onSuccess[] = $this->taskFormSucceeded;
+    return $form;
+  }
+
+  
+ /**
+ * Creating reusable-minimalistic edittask form prefilled with data
+ * 
+ */
+ 
+  protected function createComponentTaskEditForm() {
+    $form = new Nette\Application\UI\Form;
+
+    $form->addText('name', 'Meno:')
+        ->setRequired();
+
+    $form->addText('desc', 'Popis:');
+
+    $form->addText('time', 'Čas:')->setDisabled();
+
+    $form->addText('hours', 'Pridať hodin:');
+
+    $form->addText('minutes', 'Pridať minút:');
+
+
+    $form->addSubmit('send', 'Uložiť');
+    $form->onSuccess[] = $this->taskEditFormSucceeded;
+    return $form;
+  }
+
+ /**
+ * Event handler for task form
+ * 
+ */
+  public function taskFormSucceeded($form) {
+
+    $values = $form->getValues();
+
+    $task = $this->database->table('task')->insert($values);
+    $this->flashMessage('Aplikované', 'success');
+    $this->redirect('this');
+  }
+
+  
+ /**
+ * Event handler for edit form
+ * 
+ */
+  public function taskEditFormSucceeded($form) {
+
+    $values = $form->getValues();
+    $taskId = $this->getParameter('taskId');
+
+
+    $task = $this->database->table('task')->get($taskId);
+
+    $values['time'] = $task->time + $values['hours'] + ($values['minutes'] / 60);
+    unset($values['hours'], $values['minutes']);
+
+
+    $task->update($values);
+
+
+    $this->flashMessage('Aplikované', 'success');
+    $this->redirect('this');
+  }
+
+  
+  /**
+ * Presenter metod for displaying specific task
+ * 
+ */
+  public function renderShow($taskId) {
+
+    if ($this->isAjax())
+      $this->invalidateControl('content');
+
+    $task = $this->database->table('task')->get($taskId);
+    if (!$task) {
+      $this->error('Task nenajdeny');
     }
-    protected function createComponentTaskForm()
-    {
-        $form = new Nette\Application\UI\Form;
 
-        $form->addText('name', 'Názov:')
-            ->setRequired();
-
-        $form->addText('desc', 'Popis:');
-       
-       
-        
-
-
-        $form->addSubmit('send', 'Uložiť');
-        $form->onSuccess[] = $this->taskFormSucceeded;
-        return $form;
-    }
-    protected function createComponentTaskEditForm()
-    {
-        $form = new Nette\Application\UI\Form;
-
-        $form->addText('name', 'Meno:')
-            ->setRequired();
-
-        $form->addText('desc', 'Popis:');
-        
-        $form->addText('time', 'Čas:')->setDisabled();
-        
-         $form->addText('hours', 'Pridať hodin:');
-         
-         $form->addText('minutes', 'Pridať minút:');
-
-
-        $form->addSubmit('send', 'Uložiť');
-        $form->onSuccess[] = $this->taskEditFormSucceeded;
-        return $form;
-    }
-    
-    
-    
-
-
-
-
-    public function taskFormSucceeded($form)
-    {
-
-
-
-      $values = $form->getValues();
-
-        $task = $this->database->table('task')->insert($values);
-        $this->flashMessage('Aplikované', 'success');
-        $this->redirect('this');
-    }
-    
-    public function taskEditFormSucceeded($form)
-    {
-
-      $values = $form->getValues();
-      $taskId = $this->getParameter('taskId');
-        
-        
-      $task = $this->database->table('task')->get($taskId);
-      
-      $values['time'] = $task->time+$values['hours']+($values['minutes']/60);
-      unset($values['hours'], $values['minutes']);
-      
-
-          $task->update($values);
-          
-
-        $this->flashMessage('Aplikované', 'success');
-        $this->redirect('this');
-    }
-    
-    
-    public function renderShow($taskId)
-    {
-      
-            if ($this->isAjax())
-        $this->invalidateControl('content');
-      
-      $task = $this->database->table('task')->get($taskId);
-      if (!$task) {
-          $this->error('Task nenajdeny');
-      }
-
-      $this->template->task = $task;
-      
-    }
-
+    $this->template->task = $task;
+  }
 
 }
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -3,39 +3,31 @@
 namespace App\Presenters;
 
 use Nette,
-	App\Model;
-
+    App\Model;
 
 /**
- * Homepage presenter.
+ * Presenter class for displaying list of tasks
+ * @author František Hallo
  */
-class HomepagePresenter extends BasePresenter
-{
-
+class HomepagePresenter extends BasePresenter {
 
   /** @var Nette\Database\Context */
-    private $database;
+  private $database;
 
-    public function __construct(Nette\Database\Context $database)
-    {
-        $this->database = $database;
-    }
-    
-    
-//	public function renderDefault()
-//	{
-//		$this->template->anyVariable = 'any value';
-//	}
-    
-    public function renderDefault()
-{
+  public function __construct(Nette\Database\Context $database) {
+    $this->database = $database;
+  }
+
+  /**
+ * Presenter metod for displaying list of tasks ordered by newest
+ * 
+ */
+  public function renderDefault() {
     $this->template->tasks = $this->database->table('task')
-        ->order('task_id DESC')
-        ->limit(5);
-    
-          if ($this->isAjax())
-        $this->invalidateControl('content');
-      
-}
+        ->order('task_id DESC');
+
+    if ($this->isAjax())
+      $this->invalidateControl('content');
+  }
 
 }
